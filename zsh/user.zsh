2082b7c2ade87dc1b6f22e503f43dde4d004a1fa
Branden Garrett <branden.garrett@gmail.com>
2017-04-30 01:08:19 -0400

    updates

--- zsh/user.zsh
+++ zsh/user.zsh
@@ -1,2 +1,5 @@
 alias craftman="/Users/bgrrtt/.craftman/bin/craftman"
 alias v="vagrant"
+
+alias gwatch='watch -ct "git cstatus && echo &&git bc &&echo && git ll | head -n 10 &&echo && git reflog --color"'
+alias bsync='browser-sync start --proxy "localhost:8000" --files "*/*,*/*/*,*/*/*/*,*/*/*/*/*,*/*/*/*/*/*,*/*/*/*/*/*/*"'
